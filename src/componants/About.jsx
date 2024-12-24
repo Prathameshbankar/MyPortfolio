15935166ccd2917a0bcb71c8fea7c171a3ba6091
Prathameshbankar <bankarprathamesh45@gmail.com>
2024-12-24 19:36:27 +0530

    Animation Added

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import pic from "../assets/about.jpg";
 import { ABOUT_TEXT } from '../constants';
-
+import { motion } from "motion/react"
 function About() {
   return (
     <div className='border-b border-neutral-900 pb-4'>
       <h1 className='my-4 text-center text-4xl text-white'>About me</h1>
       <span className='text-neutral-500'></span>
       <div className='flex flex-wrap'>
-        <div className='w-full lg:w-1/2 lg:p-8'>
+        <motion.div
+        whileInView={{opacity:1, x:0}}
+        initial={{opacity:0, x:-100}}
+        transition={{duration:0.5}} className='w-full lg:w-1/2 lg:p-8'>
           <div className='flex items-center justify-center'>
             <img src={pic} alt="" className='rounded-lg w-1/2' />
           </div>
-        </div>
-        <div className='w-full lg:w-1/2 lg:p-8'>
+        </motion.div>
+        <motion.div
+         whileInView={{opacity:1, x:0}}
+         initial={{opacity:0, x:100}}
+         transition={{duration:0.5}}
+         className='w-full lg:w-1/2 lg:p-8'>
           <div className='flex justify-center'>
             <p className='text-justify max-w-xl my-2 py-6 max-sm:p-5'>{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { HERO_CONTENT } from '../constants';
 import pic from '../assets/Pic.jpg';
+import { motion } from "motion/react"
 
+const container = (delay) =>({
+  hidden:{x:-100, opacity:0},
+  visible:{x:0,
+    opacity:1,
+    transition : {duration:0.5, delay:delay}
+  }
+})
+ 
 function Hero() {
   return (
     <div className="mx-8 lg:mx-28 border-b border-neutral-900 pb-8 lg:pb-12">
@@ -9,21 +18,34 @@ function Hero() {
         {/* Left Content */}
         <div className="w-full lg:w-1/2 mb-8 lg:mb-0">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="text-6xl font-light tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1 
+              variants={container(0)}
+              initial = "hidden"
+              animate = "visible"
+              className="text-6xl font-light tracking-tight lg:mt-16 lg:text-8xl">
              Bankar Prathamesh
-            </h1>
-            <span className="mt-4 inline-block bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-4xl font-medium text-transparent">
+            </motion.h1>
+            <motion.span  variants={container(0.5)}
+              initial = "hidden"
+              animate = "visible" className="mt-4 inline-block bg-gradient-to-r from-pink-300 to-purple-500 bg-clip-text text-4xl font-medium text-transparent">
               Developer
-            </span>
+            </motion.span>
           </div>
-          <p className="my-4 max-w-xl py-6 font-light tracking-tight text-center lg:text-left">
+          <motion.p
+          variants={container(1)}
+          initial = "hidden"
+          animate = "visible" 
+           className="my-4 max-w-xl py-6 font-light tracking-tight text-center lg:text-left">
             {HERO_CONTENT}
-          </p>
+          </motion.p>
         </div>
 
         {/* Right Content */}
         <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
-          <img
+          <motion.img
+            initial = {{x:100, opacity:0}}
+            animate = {{x:0, opacity:1}}
+            transition={{duration:1, delay:1.2}}
             src={pic}
             alt="Profile"
             className="w-[300px] h-[300px] rounded-full object-cover shadow-lg"
