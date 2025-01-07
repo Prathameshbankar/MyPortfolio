08d51e7ee29065388212214a9278769c9d1a647f
Prathameshbankar <bankarprathamesh45@gmail.com>
2025-01-07 13:27:07 +0530

    new project added successfully

--- src/componants/Project.jsx
+++ src/componants/Project.jsx
@@ -4,6 +4,7 @@ import erp from "../assets/erp.jpg";
 import crop from "../assets/crop.jpg"
 import car from "../assets/car.jpg"
 import uber from "../assets/uber-15.svg"
+import js from "../assets/pencils.jpg"
 function Project() {
   return (
     <div className="border-b border-neutral-900 mb-24">
@@ -80,6 +81,21 @@ function Project() {
           </div>
           <p className="mb-4 text-gray-300">
           Uber Front End Clone
+          </p>
+          <button className="px-4 py-2 bg-white text-black font-semibold rounded hover:bg-yellow-400 transition">
+          Github Link
+          </button>
+        </div>
+        <div className=" text-white border-4 border-neutral-800 rounded-lg w-full sm:w-96 p-5 max-sm:mx-5 hover:transition duration-500 hover:scale-105">
+          <div className="mb-4">
+            <img
+              src={js}
+              alt="Project 1"
+              className="w-full h-52 rounded-lg"
+            />
+          </div>
+          <p className="mb-4 text-gray-300">
+          Js Mini Projects
           </p>
           <button className="px-4 py-2 bg-white text-black font-semibold rounded hover:bg-yellow-400 transition">
           Github Link
